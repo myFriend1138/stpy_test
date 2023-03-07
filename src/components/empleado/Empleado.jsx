36b1988b2d4e084132ba959baa30557f8497e645
myFriend1138 <sweetcide84@gmail.com>
2023-03-07 19:00:46 +0100

    primera version

--- src/components/empleado/Empleado.jsx
+++ src/components/empleado/Empleado.jsx
@@ -3,18 +3,18 @@ import '../empleado/empleado.css';
 
 const Empleado = ({ pic, title, text }) => {
   return (
-    <div className='gpt3__features-container__feature'>
+    <div className='stpy__features-container__feature'>
 
-      <div className='gpt3__features-container__feature-image'>
+      <div className='stpy__features-container__feature-image'>
         <img src={pic} alt='equipo'/>
       </div>
 
-      <div className='gpt3__features-container__feature-title'>
+      <div className='stpy__features-container__feature-title'>
         <div />
         <h1>{title}</h1>
       </div>
 
-      <div className='gpt3__features-container__feature-text'>
+      <div className='stpy__features-container__feature-text'>
         <p>{text}</p>
       </div>
 
